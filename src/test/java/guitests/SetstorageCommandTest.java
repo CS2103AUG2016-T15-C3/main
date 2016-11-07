6d66483cfc766fc6dbe37f1cc1e2255654e0a482
TYRA-PC\tyra <tyrack99@gmail.com>
2016-11-07 10:59:17 +0800

    add setstorageCommandTest

---
+++ src/test/java/guitests/SetstorageCommandTest.java
@@ -0,0 +1,26 @@
+package guitests;
+
+import org.junit.Test;
+import seedu.todolist.commons.core.Messages;
+import seedu.todolist.logic.commands.SetstorageCommand;
+//@@author A0158963M 
+public class SetstorageCommandTest extends ToDoListGuiTest{
+	@Test
+	public void setstorage() {
+		commandBox.runCommand("setstorage NewData");
+		assertResultMessage(SetstorageCommand.MESSAGE_SUCCESS);
+	}
+	
+	
+	@Test
+	public void setstorageInvalidPath() {
+		commandBox.runCommand("setstorage NewData*?:");
+		assertResultMessage(SetstorageCommand.MESSAGE_Invalid_Path);
+	}
+	
+	@Test
+	public void setstorageInvalidCommand() {
+		commandBox.runCommand("setstorageNewData");
+		assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+	}
+}
